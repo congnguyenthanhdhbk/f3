35644537a992be6a897a3279a3c58069e4919bec
Nguyễn Thành Công <ntcong_1@ghpfe.zone>
2020-02-18 11:01:14 +0700

    Change all module

--- f3b/src/controllers/product.controller.ts
+++
@@ -1,6 +0,0 @@
-import Product from '../models/product.model';
-import { test } from 'shelljs';
-
-export default test = (req, res) => {
-    res.send('Greetings from the Test controller!')
-}
--- f3b/tools/copyAssets.ts
+++
@@ -1,4 +0,0 @@
-import * as shell from "shelljs";
-
-// copy all the view templates
-shell.cp("-R", "src/views", "dist/")
