51c3954735aafa0ae37942bde1aea4a823a375cc
Nguyễn Thành Công <ntcong_1@ghpfe.zone>
2020-02-18 13:35:43 +0700

    Add app service

--- f3b/src/services/app.service.ts
+++ f3b/src/services/app.service.ts
@@ -2,6 +2,6 @@ import { Request, Response } from 'express';
 
 export class AppService {
     public wellcomeMessage(req: Request, res: Response) {
-        return res.status(200).send("Well come to Nguyen Thanh Cong");
+        return res.status(200).send(process.env.WELCOME_MESSAGE);
     }
 }
