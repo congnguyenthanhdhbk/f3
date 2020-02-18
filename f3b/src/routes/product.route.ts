b5314ed02d63a25247981fdf2cdeb34f7f660b7a
Nguyễn Thành Công <ntcong_1@ghpfe.zone>
2020-02-18 11:09:47 +0700

    Add nodemon

--- f3b/src/routes/product.route.ts
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ProductController = require('../controllers/product.controller');
-
-router.get('/test', ProductController.test);
-
-module.exports = router;
-
-
